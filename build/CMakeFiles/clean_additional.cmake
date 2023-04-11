758481c8f8c98c7aab1b41c3195b368180c999d8
kaipol <1335791724@qq.com>
2023-04-11 17:24:41 +0800

    first commit

---
+++ build/CMakeFiles/clean_additional.cmake
@@ -0,0 +1,10 @@
+# Additional clean files
+cmake_minimum_required(VERSION 3.16)
+
+if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "Debug")
+  file(REMOVE_RECURSE
+  "CMakeFiles\\Survivor-Game_autogen.dir\\AutogenUsed.txt"
+  "CMakeFiles\\Survivor-Game_autogen.dir\\ParseCache.txt"
+  "Survivor-Game_autogen"
+  )
+endif()
